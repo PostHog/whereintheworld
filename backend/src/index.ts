e72673d29980444882673924c998df47a85fc65a
Tim Glaser <tim@glsr.nl>
2021-09-01 16:31:37 +0100

    Remove unused import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 import csv from "csv-parser";
 import fs from "fs";
