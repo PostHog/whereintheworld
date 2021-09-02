e2e1d587ad51feddb2c7bd80ed1fd48e62364b9a
Tim Glaser <tim@glsr.nl>
2021-09-02 16:03:13 +0100

    slider

--- frontend/components/TimeTravel.tsx
+++ frontend/components/TimeTravel.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Slider from 'rc-slider'
+import { userLogic } from '../logics/userLogic'
+import { useActions } from 'kea'
+import dayjs from 'dayjs'
 
 export function TimeTravel(): JSX.Element {
     const { createSliderWithTooltip } = Slider
+    const { loadUsers } = useActions(userLogic)
 
     return (
         <div className="time-travel-wrapper">
-            <Slider min={0} max={20} defaultValue={3} />
+            <Slider min={0} max={20} defaultValue={0}  onAfterChange={(value) => loadUsers(dayjs().add(value, 'days'))}/>
         </div>
     )
 }
