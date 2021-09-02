b770f228bf6ae9acedfd7f1f34d878fbe74c4575
Tim Glaser <tim@glsr.nl>
2021-09-02 15:57:58 +0100

    add key

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -28,7 +28,7 @@ function Home(): JSX.Element {
             <div className={clsx('trip-view-wrapper', { hidden: !openTripId })}>{openTripId && <TripView />}</div>
 
             <GoogleMapReact
-                bootstrapURLKeys={{ key: '' }}
+                bootstrapURLKeys={{ key: 'AIzaSyDOV1fvZoiOUskMxIYF8sSBNgVhoCNzxsk' }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
                 fullscreenControl={false}
