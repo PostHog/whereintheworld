cdc8b7977bfb80de8c3aa3de79fe454840768600
Tim Glaser <tim@glsr.nl>
2021-09-02 15:20:41 +0100

    Whoami

--- frontend/components/WhoAmI.tsx
+++ frontend/components/WhoAmI.tsx
@@ -3,18 +3,28 @@ import React from 'react'
 import Flag from 'react-flagkit'
 import { Avatar } from './Avatar'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { useEffect } from 'react'
+import { API } from '../pages/_app'
+import { useState } from 'react'
 
 export function WhoAmI(): JSX.Element {
-    return (
+    const [profile, setProfile] = useState(false as any)
+    useEffect(() => {
+        (async () => {
+        setProfile(await(await fetch(`${API}/profile`)).json())
+        })()
+
+    }, [])
+    return profile === false ? null : (
         <div className="whoami">
             <div>
                 <Avatar
                     icon={<Flag country="ES" size={10} />}
-                    avatarUrl="https://ca.slack-edge.com/TSS5W8YQZ-UT2B67BA4-88a6594579ca-72"
+                    avatarUrl={(profile as any).picture}
                 />
             </div>
             <div>
-                James Greenhill
+                {profile.name}
                 <div className="text-muted" style={{ fontSize: '0.75em' }}>
                     <b>Barcelona, ES</b>
                 </div>
