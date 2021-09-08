98d1b9faa3f4c9aaf27e05545c3960f949310158
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-09-08 17:58:47 +0200

    remove Node backend

--- backend/prisma/migrations/20210901155333_user_is_not_unique_on_trip/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Trip.user_id_unique";
--- backend/src/controllers/utils.ts
+++
@@ -1,7 +0,0 @@
-
-export function parseIntNullable(string: string) {
-	if (isNaN(parseInt(string))) {
-			return undefined
-	}
-	return parseInt(string)
-}
