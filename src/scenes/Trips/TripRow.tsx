6dd2ce57ed6f0b3896fffc580e78a4dbedc1b697
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-25 16:56:28 +0000

    list trips 100%

--- src/scenes/Trips/TripRow.tsx
+++ src/scenes/Trips/TripRow.tsx
@@ -4,26 +4,29 @@ import Flag from 'react-flagkit'
 import { TripType } from '~/types'
 import { formatCity } from 'utils'
 import './TripRow.scss'
+import { useActions } from 'kea'
+import { tripLogic } from 'logics/tripLogic'
 
 export function TripRow({ trip }: { trip: TripType }): JSX.Element {
+    const { deleteTrip } = useActions(tripLogic)
     return (
-        <div className="trip-row">
-            <div className="col" style={{ flexGrow: 'unset' }}>
+        <tr className="trip-row">
+            <td style={{ width: 32 }}>
                 <Flag
                     country={trip.city.country.code}
                     size={32}
                     style={{ borderRadius: '50%', objectFit: 'cover', marginRight: 8 }}
                 />
-            </div>
-            <div className="col" style={{ textAlign: 'left', flexGrow: 20 }}>
-                {formatCity(trip.city)}
-            </div>
-            <div className="col" style={{ flexGrow: 4 }}>
+            </td>
+            <td style={{ textAlign: 'left' }}>{formatCity(trip.city)}</td>
+            <td>
                 {dayjs(trip.start).format('MMM DD')} - {dayjs(trip.end).format('MMM DD')}
-            </div>
-            <div className="col" style={{ flexGrow: 2 }}>
-                <span className="btn-link">Delete</span>
-            </div>
-        </div>
+            </td>
+            <td>
+                <span className="cursor-pointer text-danger" onClick={() => deleteTrip({ id: trip.id })}>
+                    Delete
+                </span>
+            </td>
+        </tr>
     )
 }
--- src/scenes/Trips/Trips.tsx
+++ src/scenes/Trips/Trips.tsx
@@ -1,19 +1,32 @@
 import { useValues } from 'kea'
+import { Button } from 'lib/components/Button'
 import { tripLogic } from 'logics/tripLogic'
 import React from 'react'
 import { TripRow } from './TripRow'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 
 export default function Trips(): JSX.Element {
     const { myTrips } = useValues(tripLogic)
     return (
         <div className="trips-scene">
-            <h1>My trips</h1>
+            <h1 className="flex-center">
+                <span style={{ flexGrow: 1 }}>My trips</span>
+                <Button size="lg">
+                    <FontAwesomeIcon icon={faPlusCircle} /> New trip
+                </Button>
+            </h1>
             <div style={{ marginBottom: 32 }}>A journey of a thousand miles begins with a single step.</div>
 
-            {myTrips.length === 0 && "You don't have any upcoming trips. Time to add one?"}
-            {myTrips.map((trip) => (
-                <TripRow key={trip.id} trip={trip} />
-            ))}
+            {myTrips.length === 0 ? (
+                "You don't have any upcoming trips. Time to add one?"
+            ) : (
+                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                    {myTrips.map((trip) => (
+                        <TripRow key={trip.id} trip={trip} />
+                    ))}
+                </table>
+            )}
         </div>
     )
 }
